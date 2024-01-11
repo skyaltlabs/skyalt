08404cbdaa8f2fe98cadeaf3a768ab53b7c23340
milansuk <milansuk86@gmail.com>
2024-01-11 19:00:02 +0100

    Fixed: Text/Editbox vertical and horizontal scroll

--- ui_paint_text.go
+++ ui_paint_text.go
@@ -62,7 +62,9 @@ func (ui *Ui) Paint_textGrid(grid OsV4, cd OsCd, style *UiComp, value string, va
 	lv.call.data.scrollV.show = false
 
 	ui.Div_col(grid.Start.X, OsMaxFloat(ui.DivInfo_get(SA_DIV_GET_layoutWidth, 0), ui.Paint_textWidth(value, -1, 0, "", style.label_formating))) //+marginX*4+margin*2
-	ui.Div_row(grid.Start.Y, ui.DivInfo_get(SA_DIV_GET_layoutHeight, 0))
+	ui.Div_row(grid.Start.Y, 0.5)
+	ui.Div_rowMax(grid.Start.Y, 100)
+
 	ui.Div_start(grid.Start.X, grid.Start.Y, grid.Size.X, grid.Size.Y)
 	//style.Paint(st.stack.canvas, value, valueOrigEdit, selection, edit, icon, icon_margin, enable, app)
 
